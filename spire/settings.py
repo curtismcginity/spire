137072c4e308c06dee6504a9b002721e02f3efbf
curtismcginity <scmcginity@gmail.com>
2017-10-05 15:58:02 -0400

    static files settings updated

--- spire/settings.py
+++ spire/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # for static files
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
+
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT,'static'),
 )
