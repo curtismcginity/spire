e4fa31d4d213a4a6bb1b0a54f07902dd6e7c78b7
curtismcginity <scmcginity@gmail.com>
2017-10-06 01:11:19 -0400

    converging..

--- spire/settings.py
+++ spire/settings.py
@@ -88,14 +88,15 @@ WSGI_APPLICATION = 'spire.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+
 
 
 # Password validation
